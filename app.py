eabced0c2b3215e036cbaa571ef07cc2eb0b14b0
netoguindani-design <netoguindani@hotmail.com>
2026-03-01 07:49:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -101,5 +101,6 @@ if btn_ativar:
                 st.session_state.historico_precos.append(preco_atual)
             
             time.sleep(60) # Espera 1 minuto para o próximo ciclo
-MEU_WHATSAPP = "+5593981292787"
-API_KEY_WHATSAPP = “9580681” #
+def enviar_mensagem():
+    MEU_WHATSAPP = "+5593981292787"
+    API_KEY_WHATSAPP = “9580681” 
